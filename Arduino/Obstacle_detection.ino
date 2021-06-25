2aee1d65503be9753c915cc417460beef666580a
hareeshrp <69135364+hareeshrp@users.noreply.github.com>
2021-06-25 15:43:12 +0530

    Add files via upload

---
+++ Arduino/Obstacle_detection.ino
@@ -0,0 +1,31 @@
+#include"Ultrasonic.h"
+Ultrasonic ultrasonic(6,7);
+const int light = 13;
+long microsec = 0;
+float distance = 0;
+
+void setup
+{
+    Serial.begin(9600);
+    pinMode(light,OUTPUT);
+}
+
+void loop()
+{
+    microsec = ultrasonic.timing();
+    distance = ultrasonic.convert(microsec,Ultrasonic::CM);
+    led ultra();
+    Serial.print(distance);
+    Serial.println("cm");
+    delay(1000);
+}
+
+void ultra()
+{
+    digitalWrite(light,LOW);
+    if (distance > 5)
+    {
+        digitalWrite(light,HIGH);
+    }
+}
+
